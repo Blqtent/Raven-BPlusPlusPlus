44b2dbda78fe84d41dd15bb1b71fffc9fa79631f
Cosmic-SC <133380776+Cosmic-SC@users.noreply.github.com>
2023-06-10 00:17:36 +0400

    raycasting

--- src/main/java/keystrokesmod/client/utils/CombatUtils.java
+++ src/main/java/keystrokesmod/client/utils/CombatUtils.java
@@ -1,5 +1,7 @@
 package keystrokesmod.client.utils;
 
+import com.google.common.base.Predicates;
+import keystrokesmod.client.module.modules.combat.aura.KillAura;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -7,6 +9,11 @@ import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.*;
+
+import java.util.List;
+
+import static keystrokesmod.client.utils.Utils.mc;
 
 public class CombatUtils {
     public static boolean canTarget(Entity entity, boolean idk) {
@@ -53,5 +60,68 @@ public class CombatUtils {
             return true;
         }
     }
+    public static Entity raycastEntity(final double range, final IEntityFilter entityFilter) {
+        return raycastEntity(range, KillAura.yaw, KillAura.pitch,
+                entityFilter);
+    }
+    public static boolean canEntityBeSeen(Entity entityIn) {
+        EntityPlayer p = mc.thePlayer;
+        return mc.theWorld.rayTraceBlocks(new Vec3(p.posX, p.posY + (double) p.getEyeHeight(), p.posZ),
+                new Vec3(entityIn.posX, entityIn.posY + (double) entityIn.getEyeHeight(), entityIn.posZ), false) == null;
+    }
+    private static Entity raycastEntity(final double range, final float yaw, final float pitch, final IEntityFilter entityFilter) {
+        final Entity renderViewEntity = mc.getRenderViewEntity();
 
+        if (renderViewEntity != null && mc.theWorld != null) {
+            double blockReachDistance = range;
+            final Vec3 eyePosition = renderViewEntity.getPositionEyes(1F);
+
+            final float yawCos = MathHelper.cos(-yaw * 0.017453292F - (float) Math.PI);
+            final float yawSin = MathHelper.sin(-yaw * 0.017453292F - (float) Math.PI);
+            final float pitchCos = -MathHelper.cos(-pitch * 0.017453292F);
+            final float pitchSin = MathHelper.sin(-pitch * 0.017453292F);
+
+            final Vec3 entityLook = new Vec3(yawSin * pitchCos, pitchSin, yawCos * pitchCos);
+            final Vec3 vector = eyePosition.addVector(entityLook.xCoord * blockReachDistance, entityLook.yCoord * blockReachDistance, entityLook.zCoord * blockReachDistance);
+            final List<Entity> entityList = mc.theWorld.getEntitiesInAABBexcluding(renderViewEntity, renderViewEntity.getEntityBoundingBox().addCoord(entityLook.xCoord * blockReachDistance, entityLook.yCoord * blockReachDistance, entityLook.zCoord * blockReachDistance).expand(1D, 1D, 1D), Predicates.and(EntitySelectors.NOT_SPECTATING, Entity::canBeCollidedWith));
+
+            Entity pointedEntity = null;
+
+            for (final Entity entity : entityList) {
+                if (!entityFilter.canRaycast(entity))
+                    continue;
+
+                final float collisionBorderSize = entity.getCollisionBorderSize();
+                final AxisAlignedBB axisAlignedBB = entity.getEntityBoundingBox().expand(collisionBorderSize, collisionBorderSize, collisionBorderSize);
+                final MovingObjectPosition movingObjectPosition = axisAlignedBB.calculateIntercept(eyePosition, vector);
+
+                if (axisAlignedBB.isVecInside(eyePosition)) {
+                    if (blockReachDistance >= 0.0D) {
+                        pointedEntity = entity;
+                        blockReachDistance = 0.0D;
+                    }
+                } else if (movingObjectPosition != null) {
+                    final double eyeDistance = eyePosition.distanceTo(movingObjectPosition.hitVec);
+
+                    if (eyeDistance < blockReachDistance || blockReachDistance == 0.0D) {
+                        if (entity == renderViewEntity.ridingEntity && !renderViewEntity.canRiderInteract()) {
+                            if (blockReachDistance == 0.0D)
+                                pointedEntity = entity;
+                        } else {
+                            pointedEntity = entity;
+                            blockReachDistance = eyeDistance;
+                        }
+                    }
+                }
+            }
+
+            return pointedEntity;
+        }
+
+        return null;
+    }
+
+    public interface IEntityFilter {
+        boolean canRaycast(final Entity entity);
+    }
 }
