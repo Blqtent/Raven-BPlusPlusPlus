63e61633617caedae0edb73a9d18d7f643b03c33
Cosmic-SC <133380776+Cosmic-SC@users.noreply.github.com>
2023-06-10 14:10:48 +0400

    AHHHHHHHHHH K-OV WHAT DID U DO TO THIS!?!?!?!??
    for some reason Blocks module just does not wanna work correctly sometimes

--- src/main/java/keystrokesmod/client/module/modules/hotkey/Blocks.java
+++ src/main/java/keystrokesmod/client/module/modules/hotkey/Blocks.java
@@ -1,15 +1,17 @@
 package keystrokesmod.client.module.modules.hotkey;
 
 import keystrokesmod.client.module.Module;
+import keystrokesmod.client.module.setting.impl.DescriptionSetting;
 import keystrokesmod.client.utils.Utils;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
-public class Blocks extends Module { //DONT DELETE THIS ITS USEFULL
+public class Blocks extends Module { //DONT DELETE THIS ITS USEFUL
 
     public Blocks() {
         super("Blocks", ModuleCategory.hotkey);
+        this.registerSetting(new DescriptionSetting("Hotkey for blocks"));
     }
 
     @Override
@@ -18,21 +20,41 @@ public class Blocks extends Module { //DONT DELETE THIS ITS USEFULL
             return;
         if (isPlayerHoldingBlocks()) {
             this.disable();
+            return;
         }
-        for (int slot = 0; slot <= 8; slot++) {
-            ItemStack itemInSlot = mc.thePlayer.inventory.getStackInSlot(slot);
-            if (itemInSlot != null && itemInSlot.getItem() instanceof ItemBlock && !itemInSlot.getItem().getRegistryName().equalsIgnoreCase("minecraft:tnt") && (((ItemBlock) itemInSlot.getItem()).getBlock().isFullBlock() || ((ItemBlock) itemInSlot.getItem()).getBlock().isFullCube())) {
-                if (mc.thePlayer.inventory.currentItem != slot) {
-                    mc.thePlayer.inventory.currentItem = slot;
-                } else {
+
+        if (!isPlayerHoldingBlocks()) {
+            for (int slot = 0; slot <= 8; slot++) {
+                ItemStack itemInSlot = mc.thePlayer.inventory.getStackInSlot(slot);
+                if (itemInSlot != null && itemInSlot.getItem() instanceof ItemBlock && !itemInSlot.getItem().getRegistryName().equalsIgnoreCase("minecraft:tnt") && (((ItemBlock) itemInSlot.getItem()).getBlock().isFullBlock() || ((ItemBlock) itemInSlot.getItem()).getBlock().isFullCube())) {
+                    if (mc.thePlayer.inventory.currentItem != slot) {
+                        mc.thePlayer.inventory.currentItem = slot;
+                    }
+                    this.disable();
                     return;
                 }
-                this.disable();
-                return;
+            }
+            this.disable();
+        }
+    }
+
+    @Override
+    public void onDisable(){
+        if (!Utils.Player.isPlayerInGame())
+            return;
+        if (isPlayerHoldingBlocks()) {
+            return;
+        }
+        if (!isPlayerHoldingBlocks()) {
+            for (int slot = 0; slot <= 8; slot++) {
+                ItemStack itemInSlot = mc.thePlayer.inventory.getStackInSlot(slot);
+                if (itemInSlot != null && itemInSlot.getItem() instanceof ItemBlock && !itemInSlot.getItem().getRegistryName().equalsIgnoreCase("minecraft:tnt") && (((ItemBlock) itemInSlot.getItem()).getBlock().isFullBlock() || ((ItemBlock) itemInSlot.getItem()).getBlock().isFullCube())) {
+                    if (mc.thePlayer.inventory.currentItem != slot) {
+                        mc.thePlayer.inventory.currentItem = slot;
+                    }
+                }
             }
         }
-        this.disable();
-
     }
     public static boolean isPlayerHoldingBlocks() {
         if (mc.thePlayer.getCurrentEquippedItem() == null) {
