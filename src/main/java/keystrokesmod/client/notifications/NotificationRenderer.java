c7aa5ca5450dca71fe0d46948040f60137727315
Cosmic-SC <133380776+Cosmic-SC@users.noreply.github.com>
2023-06-10 14:03:14 +0400

    less annoying

--- src/main/java/keystrokesmod/client/notifications/NotificationRenderer.java
+++ src/main/java/keystrokesmod/client/notifications/NotificationRenderer.java
@@ -7,6 +7,7 @@ import keystrokesmod.client.event.impl.Render2DEvent;
 import keystrokesmod.client.main.ClientConfig;
 import keystrokesmod.client.module.Module;
 import keystrokesmod.client.module.modules.client.GuiModule;
+import keystrokesmod.client.module.modules.hotkey.Blocks;
 import keystrokesmod.client.utils.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -26,7 +27,7 @@ public class NotificationRenderer {
                 || ClientConfig.applyingConfig)
             return;
 
-        if (!m.getClass().equals(Gui.class)) {
+        if (!m.getClass().equals(Blocks.class)) {
             String s = m.isEnabled() ? "enabled" : "disabled";
             NotificationManager
                     .show(new Notification(NotificationType.INFO, "Module " + s, m.getName() + " has been " + s, 1));
