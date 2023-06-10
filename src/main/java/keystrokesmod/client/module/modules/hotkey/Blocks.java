c5218d6ac13d67ac4d6ee9559e165a78f8959e47
Cosmic-SC <133380776+Cosmic-SC@users.noreply.github.com>
2023-06-10 13:20:47 +0400

    fix blocks module

--- src/main/java/keystrokesmod/client/module/modules/hotkey/Blocks.java
+++ src/main/java/keystrokesmod/client/module/modules/hotkey/Blocks.java
@@ -1,44 +1,27 @@
 package keystrokesmod.client.module.modules.hotkey;
 
 import keystrokesmod.client.module.Module;
-import keystrokesmod.client.module.setting.impl.SliderSetting;
-import keystrokesmod.client.module.setting.impl.TickSetting;
 import keystrokesmod.client.utils.Utils;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
-public class Blocks extends Module {
-    private final TickSetting preferSlot;
-    private final SliderSetting hotbarSlotPreference;
+public class Blocks extends Module { //DONT DELETE THIS ITS USEFULL
 
     public Blocks() {
         super("Blocks", ModuleCategory.hotkey);
-
-        this.registerSetting(preferSlot = new TickSetting("Prefer a slot", false));
-        this.registerSetting(hotbarSlotPreference = new SliderSetting("Prefer wich slot", 9, 1, 9, 1));
     }
 
     @Override
     public void onEnable() {
         if (!Utils.Player.isPlayerInGame())
             return;
-
-        if (preferSlot.isToggled()) {
-            int preferedSlot = (int) hotbarSlotPreference.getInput() - 1;
-
-            ItemStack itemInSlot = mc.thePlayer.inventory.getStackInSlot(preferedSlot);
-            if (itemInSlot != null && itemInSlot.getItem() instanceof ItemBlock) {
-                mc.thePlayer.inventory.currentItem = preferedSlot;
-                this.disable();
-                return;
-            }
+        if (isPlayerHoldingBlocks()) {
+            this.disable();
         }
-
         for (int slot = 0; slot <= 8; slot++) {
             ItemStack itemInSlot = mc.thePlayer.inventory.getStackInSlot(slot);
-            if (itemInSlot != null && itemInSlot.getItem() instanceof ItemBlock
-                    && (((ItemBlock) itemInSlot.getItem()).getBlock().isFullBlock()
-                            || ((ItemBlock) itemInSlot.getItem()).getBlock().isFullCube())) {
+            if (itemInSlot != null && itemInSlot.getItem() instanceof ItemBlock && !itemInSlot.getItem().getRegistryName().equalsIgnoreCase("minecraft:tnt") && (((ItemBlock) itemInSlot.getItem()).getBlock().isFullBlock() || ((ItemBlock) itemInSlot.getItem()).getBlock().isFullCube())) {
                 if (mc.thePlayer.inventory.currentItem != slot) {
                     mc.thePlayer.inventory.currentItem = slot;
                 } else {
@@ -49,5 +32,14 @@ public class Blocks extends Module {
             }
         }
         this.disable();
+
+    }
+    public static boolean isPlayerHoldingBlocks() {
+        if (mc.thePlayer.getCurrentEquippedItem() == null) {
+            return false;
+        } else {
+            Item item = mc.thePlayer.getCurrentEquippedItem().getItem();
+            return item instanceof ItemBlock;
+        }
     }
 }
