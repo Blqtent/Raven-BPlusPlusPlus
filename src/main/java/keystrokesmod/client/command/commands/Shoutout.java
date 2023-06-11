86c65bbb34952dccc06f982500c1ee2346d60af7
Stephen <123317160+StephenIsTaken@users.noreply.github.com>
2023-06-11 14:37:19 +0300

    Update Shoutout.java

--- src/main/java/keystrokesmod/client/command/commands/Shoutout.java
+++ src/main/java/keystrokesmod/client/command/commands/Shoutout.java
@@ -6,16 +6,16 @@ import static keystrokesmod.client.clickgui.raven.Terminal.print;
 
 public class Shoutout extends Command {
     public Shoutout() {
-        super("shoutout", "Everyone who helped make b+", 0, 0, new String[] {}, new String[] { "love", "thanks" });
+        super("shoutout", "Everyone who helped make b+++", 0, 0, new String[] {}, new String[] { "love", "thanks" });
     }
 
     @Override
     public void onCall(String[] args) {
         print("Everyone who made b++ possible:");
-        print("- kv! aka KingVoid (current dev)");
+        print("- Comsic-SC (most bypasses)");
         print("- kopamed (raven b+ dev)");
         print("- hevex/blowsy (weeaboo, b3 dev) (disapproves to b+ as he earned less money because less ppl clicked on his adfly link)");
-        print("- blowsy (hevex's alt)");
+        print("- StephenIsTaken (me)");
         print("- jmraichdev (client dev)");
         print("- nighttab (website dev)");
         print("- mood (java help)");
