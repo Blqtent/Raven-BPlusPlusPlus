9371ca19318772667a2d1f0047e766fe53972359
Cosmic-SC <133380776+Cosmic-SC@users.noreply.github.com>
2023-06-11 15:40:15 +0400

    better

--- src/main/java/keystrokesmod/client/command/commands/Shoutout.java
+++ src/main/java/keystrokesmod/client/command/commands/Shoutout.java
@@ -11,14 +11,15 @@ public class Shoutout extends Command {
 
     @Override
     public void onCall(String[] args) {
-        print("Everyone who made b++ possible:");
+        print("Everyone who made b+++ possible:");
+        print("- sigmaclientwastaken (raven b++ dev)");
+        print("- k-ov (raven b++ dev)");
         print("- Comsic-SC (most bypasses)");
         print("- kopamed (raven b+ dev)");
         print("- hevex/blowsy (weeaboo, b3 dev) (disapproves to b+ as he earned less money because less ppl clicked on his adfly link)");
-        print("- StephenIsTaken (me)");
+        print("- StephenIsTaken (main dev (still learning lmao))");
         print("- jmraichdev (client dev)");
         print("- nighttab (website dev)");
         print("- mood (java help)");
-        print("- jc (b3 b2 betta tester, very good moaner (moans very loudly in discord vcs, giving everyone emotional motivation))");
     }
 }
