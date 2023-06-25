fc4bff5a8cd5c221e2544312a7a2fd39bbe240a2
Cosmic-SC <133380776+Cosmic-SC@users.noreply.github.com>
2023-06-25 19:13:44 +0400

    NoHurtCam module

---
+++ src/main/java/keystrokesmod/client/module/modules/other/NoHurtCam.java
@@ -0,0 +1,9 @@
+package keystrokesmod.client.module.modules.other;
+
+import keystrokesmod.client.module.Module;
+
+public class NoHurtCam extends Module {
+    public NoHurtCam(){
+        super("NoHurtCam",ModuleCategory.other);
+    }
+}
